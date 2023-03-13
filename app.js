e986c81da5b3acbfb95dd3305818689a7254777e
Vivek Shukla <vivekshuklahere@gmail.com>
2023-03-13 09:05:45 +0530

    Updated APp.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
+// supabase
+const supabase = require("./config/supabaseConfig");
+// console.log(supabase)
+
+// controllers
+const userRoute = require("./routes/userRoute");
+
 const express = require("express");
 const app = express();
 
+app.use("/users", userRoute);
+
 app.get("/", (req, res) => {
-  res.send("ListFlow");
+  res.send("ListFlow Work Management Tool");
 });
 
 const port = process.env.PORT || 3000;
